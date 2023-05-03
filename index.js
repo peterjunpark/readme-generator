e0365bb2a17e1c9b76cfc474ee8d215832672f6b
Peter Park <peterjpark@pm.me>
2023-05-03 16:32:01 -0400

    Update questions

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const greenify = text => `\x1b[32m${text}\x1b[0m\x1b[1m`
+
 // TODO: Create an array of questions for user input
 const questions = [
   {
     name: "title",
     type: "input",
-    message: "Enter the title of your project.",
+    message: `The ${greenify("title")} of your project:`,
+  },
+  {
+    name: "repo",
+    type: "input",
+    message: `The ${greenify("name of your project's GitHub repository")}:`,
   },
   {
     name: "username",
     type: "input",
-    message: "What is your GitHub username?",
+    message: `Your ${greenify("GitHub username")}:`,
   },
   {
     name: "email",
     type: "input",
-    message: "What is your email address?",
+    message: `Your ${greenify("email address")}:`,
   },
   {
     name: "description",
     type: "input",
-    message: "Enter a description of your project.",
+    message: `A brief ${greenify("description")} of your project:`,
   },
   {
     name: "installation",
     type: "input",
-    message: "Explain how to install your project.",
+    message: `${greenify("Installation instructions")} to get started with your project:`,
   },
   {
     name: "usage",
     type: "input",
-    message: "Describe how to use your project.",
+    message: `Instructions and examples on ${greenify("how to use")} your project:`,
   },
   {
     name: "contribution",
     type: "input",
-    message: "Enter guidelines on how to contribute to your project.",
+    message: `${greenify("Contribution guidelines")} for your project:`,
   },
   {
     name: "tests",
     type: "input",
-    message: "",
+    message: `Instructions on ${greenify("how to run tests")} included with your project:`,
   },
   {
     name: "license",
     type: "list",
-    message: "Which license is your project covered under?",
+    message: `The ${greenify("license")} your project is covered under:`,
     choices: [
       "Apache License 2.0",
       "GNU GPL v2",
