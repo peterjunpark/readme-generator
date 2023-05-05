524ae9d67a1a27155eacb8d604a2701ac67d0e37
Peter Park <peterjpark@pm.me>
2023-05-05 19:06:40 -0400

    Add writeToFile func

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-const greenify = text => `\x1b[32m${text}\x1b[0m\x1b[1m`
-
-// TODO: Create an array of questions for user input
+const greenify = (text) => `\x1b[32m${text}\x1b[0m\x1b[1m`;
 const questions = [
   {
     name: "title",
     type: "input",
     message: `The ${greenify("title")} of your project:`,
-  },
-  {
-    name: "repo",
-    type: "input",
-    message: `The ${greenify("name of your project's GitHub repository")}:`,
   },
   {
     name: "username",
@@ -24,6 +18,10 @@ const questions = [
     name: "email",
     type: "input",
     message: `Your ${greenify("email address")}:`,
+    validate: answer => {
+      if (answer && (!answer.includes("@") || !answer.includes("."))) return "Please enter a valid email address.";
+      return true;
+    }
   },
   {
     name: "description",
@@ -33,22 +31,26 @@ const questions = [
   {
     name: "installation",
     type: "input",
-    message: `${greenify("Installation instructions")} to get started with your project:`,
+    message: `Instructions on ${greenify("how to install")} your project:`,
   },
   {
     name: "usage",
     type: "input",
-    message: `Instructions and examples on ${greenify("how to use")} your project:`,
+    message: `Instructions and examples on ${greenify(
+      "how to use"
+    )} your project:`,
   },
   {
-    name: "contribution",
+    name: "contribute",
     type: "input",
-    message: `${greenify("Contribution guidelines")} for your project:`,
+    message: `Guidelines on ${greenify("contributing")} to your project:`,
   },
   {
     name: "tests",
     type: "input",
-    message: `Instructions on ${greenify("how to run tests")} included with your project:`,
+    message: `Instructions on ${greenify(
+      "how to run tests"
+    )} included with your project:`,
   },
   {
     name: "license",
@@ -60,19 +62,34 @@ const questions = [
       "GNU GPL v3",
       "ISC License",
       "MIT License",
-    ]
+    ],
+  },
+  {
+    name: "confirm",
+    type: "confirm",
+    message: "\x1b[35mAre the above details correct?\x1b[0m",
   },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+    console.error(err);
+  })
+}
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions)
-  .then(answers => {
-    console.log(answers);
-  })
+  console.log(
+    "\x1b[1m\x1b[35m|==========| README Generator |==========|\x1b[0m"
+  );
+  console.log(
+    "\x1b[95mUse the following prompts to generate a README.md for your project."
+  );
+  inquirer.prompt(questions).then((answers) => {
+    const content = generateMarkdown(answers);
+    writeToFile("./README.md", content);
+  });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return license ? `https://img.shields.io/badge/License-${license}-informational` : "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return license ? `[![License](${renderLicenseBadge(license)})](#License)` : "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown({
+  title,
+  username,
+  email,
+  desc,
+  installation,
+  usage,
+  contributing,
+  tests,
+  license
+}) {
+  let readme = `# ${title}\n## Table of Contents\n`;
+  return readme;
 }
 
 module.exports = generateMarkdown;
